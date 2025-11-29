3053c8110828ee90cc10af82a3374ae5e4f85058
SCIPIO666 <eaphoney@gmail.com>
2025-11-29 12:41:10 +0300

    circle function

--- script.js
+++ script.js
@@ -1,30 +1,76 @@
+
 const canvas=document.getElementById("asteroids");
 const context= canvas.getContext("2d");
 
- context.strokeStyle ="#00ff00";
- context.fillStyle="grey"
- context.lineWidth = 0.25;
+function drawGrid(ctx, minorSize, majorSize, stroke, fill) {
+    const minor = minorSize || 10;
+    const major = majorSize || minor * 5;
 
- /////drawing a grid/////////
-for(let x=0; x<canvas.width; x+=10){
-    context.moveTo(x,0);
-    context.lineTo(x, canvas.height);
+    const strokeStyle = stroke || "#00FF00";
+    const fillStyle = fill || "#009900"; 
+    
+    ctx.save();
+    ctx.strokeStyle = strokeStyle;
+    ctx.fillStyle = fillStyle;
+    ctx.font = '10px Arial';
+
+    let width = ctx.canvas.width;
+    let height = ctx.canvas.height;
+    
+    for(let x = 0; x < width; x += minor) {
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, height);
+        ctx.lineWidth = (x % major == 0) ? 0.5 : 0.25;
+        ctx.stroke();
+        
+        if(x % major == 0 && x !== 0) { 
+            ctx.fillText(x, x + 2, 10); 
+        }
+    }
+    
+    for(let y = 0; y < height; y += minor) {
+        ctx.beginPath();
+        ctx.moveTo(0, y);
+        ctx.lineTo(width, y);
+        ctx.lineWidth = (y % major == 0) ? 0.5 : 0.25;
+        ctx.stroke();
+        
+        if(y % major == 0 && y !== 0) { 
+            ctx.fillText(y, 2, y + 10); 
+        }
+    }
+    ctx.restore();
 }
 
-for(let y=0; y<canvas.width; y+=10){
-    context.moveTo(0,y);
-    context.lineTo(canvas.width, y);
-}
-
-context.stroke();
+drawGrid(context, 10, 50, "#00FF00", "#009900");
 ////////////////////////////////////////
 
-//draw 3 circles
+
+
+//draw pacman
+
+function drawCircle(context,centrePoint,radius,beginAngle,EndAngle,stroke, fill){
+
+    const strokeStyle = stroke || "#00FF00";
+    const fillStyle = fill || "#009900"; 
+
+    const midpoint= centrePoint || "100,150";
+    const circleRadius= radius || 50;
+    const arcStartPoint=beginAngle ||  0;
+    const arcEndPoint=EndAngle || 2;
 
 context.beginPath()
-context.strokeStyle="blue"
-context.fillStyle="red"
-context.moveTo(400,150)
-context.arc(400,150,50,Math.PI*0.2,Math.PI*1.8)
-context.lineTo(400,150)
-context.fill()
+context.strokeStyle=strokeStyle;
+context.fillStyle=fillStyle;
+context.moveTo(midpoint);
+context.arc(`${midpoint,circleRadius,Math.PI*arcStartPoint,Math.PI*arcEndPoint}`);
+context.lineTo(midpoint);
+context.stroke()
+//context.fill();
+
+
+}
+
+
+drawCircle(context,"100,100",30,0.6,1.2,"blue","red")
